af1c6f19cf5e1b8c23f98cdadfca36f98c81839b
felli87 <felli.c87@gmail.com>
2023-04-02 21:15:29 +0300

    add galery beta

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//Pop-up window---------------------------------
+
 const contactBtn = document.querySelector('.under-header-section__button');
 const popupWindow = document.querySelector('.popup-window');
 const cancelBtn = document.querySelector('.popup-window__cancel-button');
 const popupBg = document.querySelector('.popup-window__bg');
+
 
 contactBtn.addEventListener('click', function() {
   popupWindow.classList.add('no-hide');
@@ -13,3 +16,38 @@ cancelBtn.addEventListener('click', function() {
   popupWindow.classList.remove('no-hide');
   popupBg.classList.remove('no-hide');
 });
+//Pop-up window--------------------------------
+
+//zoom img-------------------------------------
+const greyBg = document.querySelector('.grey-bg');
+const photoGaleryPanel = document.querySelector('.photo-galery-panel');
+const photoGaleryPanelCloseBtn = document.querySelector('.photo-galery-panel__close-btn');
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var images = document.querySelectorAll(".zoomable");
+
+  images.forEach(function(image) {
+    image.addEventListener("click", function() {
+      image.classList.add("item-photo-zoom-styles")
+      greyBg.style.display = "block"
+      photoGaleryPanel.style.display = "flex"
+    });
+  });
+});
+
+photoGaleryPanelCloseBtn.addEventListener('click', function() {
+  itemPhotoZoomStyles = document.querySelector('.item-photo-zoom-styles');
+  itemPhotoZoomStyles.classList.remove("item-photo-zoom-styles")
+  greyBg.style.display = "none"
+  photoGaleryPanel.style.display = "none"
+
+
+});
+
+
+//zoom img--------------------------------------
+
+
+
